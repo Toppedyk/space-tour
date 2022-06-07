f9fbc76272542d5e5221f129151cd36f5e0f3e0b
taylor.oppedyk <taylor.oppedyk@allata.com>
2022-06-07 08:48:29 -0600

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrewComponent } from './Pages/crew/crew.component';
+import { DestinationComponent } from './Pages/destination/destination.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { TechnologyComponent } from './Pages/technology/technology.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "destination", component: DestinationComponent},
+  {path: "crew", component: CrewComponent},
+  {path: "technology", component: TechnologyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { DestinationComponent } from './Pages/destination/destination.component';
+import { CrewComponent } from './Pages/crew/crew.component';
+import { TechnologyComponent } from './Pages/technology/technology.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DestinationComponent,
+    CrewComponent,
+    TechnologyComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
