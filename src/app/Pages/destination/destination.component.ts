e3eda041c751783090ec95185bd9a7e1a83f6fe9
taylor.oppedyk <taylor.oppedyk@allata.com>
2022-06-09 13:08:50 -0600

    destination functionality done

--- src/app/Pages/destination/destination.component.ts
+++ src/app/Pages/destination/destination.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Destination } from 'src/app/Models/Destination';
+import { data } from 'src/app/starter-code/data';
 
 @Component({
   selector: 'app-destination',
   templateUrl: './destination.component.html',
-  styleUrls: ['./destination.component.css']
+  styleUrls: ['./destination.component.css'],
 })
 export class DestinationComponent implements OnInit {
+  destinations: Destination[] = data['destinations'];
+  activeDestination: Destination = {
+    name: '',
+    images: {
+      png: '',
+      webp: '',
+    },
+    description:
+      '',
+    distance: '',
+    travel: '',
+  };
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.destinations);
+    this.activeDestination = this.destinations[0]
+    console.log(this.activeDestination);
   }
 
+  changeActive(name:string) {
+    var newDestination = this.destinations.filter(d => d.name == name)
+    this.activeDestination = newDestination[0];
+  }
 }
