8d85b488c816b70892a02b86a14f71065442b00a
taylor.oppedyk <taylor.oppedyk@allata.com>
2022-06-09 15:45:33 -0600

    Starting Tech

--- src/app/Pages/technology/technology.component.ts
+++ src/app/Pages/technology/technology.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Technology } from 'src/app/Models/Technology';
+import { data } from 'src/app/starter-code/data';
 
 @Component({
   selector: 'app-technology',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./technology.component.css']
 })
 export class TechnologyComponent implements OnInit {
+  technology: Technology[] = data["technology"]
+  activeTech: Technology = {
+  name: "",
+  images: {
+    portrait: "",
+    landscape: ""
+  },
+  description: ""
+}
 
   constructor() { }
 
   ngOnInit(): void {
+    this.activeTech=this.technology[0]
   }
 
+  changeActive(name: string) {
+    var newTech = this.technology.filter(t => t.name == name)
+    this.activeTech = newTech[0]
+  }
 }
